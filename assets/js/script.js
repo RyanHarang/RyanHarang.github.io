aac4c11f9c2d1b0509ea2fc050d43ab73cb08e41
Ryan <harangr@wwu.edu>
2023-05-04 19:51:34 -0700

    Mobile Dropdown implemented, doesn't work yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-function switchMode() {
-    var el1 = document.getElementById("home");
-    console.log("Mode clicked");
-    el1.classList.toggle("light-mode");
+function dropDown() {
+    document.getElementById("mobileDrop").classList.toggle("show");
 }
