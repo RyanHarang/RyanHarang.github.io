62b96b1cf4c1f6267ca23be272ac1ab24c76917a
Ryan <120701017+RyanHarang@users.noreply.github.com>
2023-05-31 22:40:53 -0700

    added anime.js library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 function dropDown() {
-    document.getElementById("mobileDrop").classList.toggle("show");
+  document.getElementById("mobileDrop").classList.toggle("show");
 }
+
+anime({
+  targets: ".dropdown",
+  translateX: 30,
+});
+
+anime({
+  targets: ".",
+  translateX: 30,
+});
