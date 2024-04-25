39e6ee728a06167ff670b4f7bb4e17cab7ceec01
Ryan <ryh110.rh@gmail.com>
2024-04-24 21:56:04 -0700

    moved export for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
+
 // Importing Components
 import UpWave from "./components/sub-components/UpWave";
 import DownWave from "./components/sub-components/DownWave";
 import UpButton from "./components/sub-components/UpButton";
 
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Intro from "./components/Intro";
 import About from "./components/About";
 import ProjectSection from "./components/ProjectSection";
@@ -12,27 +15,34 @@ import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="overflow-hidden bg-dark-100 dark:bg-dark-900 duration-500 text-dark-800 dark:text-dark-100">
-      <Header />
-      <hr className="md:py-32 py-20" />
-      <Intro />
-      <UpWave />
-      <About />
-      <DownWave />
-      <ProjectSection />
-      <UpWave />
-      <Skills />
-      <DownWave />
-      <Experience />
-      <UpWave />
-      <Contact />
-      <DownWave />
-      <UpButton />
-      <Footer />
-    </div>
+    <>
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className="overflow-hidden bg-dark-100 dark:bg-dark-900 duration-500 text-dark-800 dark:text-dark-100">
+        <Header />
+        <hr className="md:py-32 py-20" />
+        <Intro />
+        <UpWave />
+        <About />
+        <DownWave />
+        <ProjectSection />
+        <UpWave />
+        <Skills />
+        <DownWave />
+        <Experience />
+        <UpWave />
+        <Contact />
+        <DownWave />
+        <UpButton />
+        <Footer />
+      </div>
+    </>
   );
 }
-
-export default App;
