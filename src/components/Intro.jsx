87891b3caffe87ae24b87fc0ac5d5793b8dec868
Ryan <ryh110.rh@gmail.com>
2024-06-03 13:45:29 -0700

    removed javascript, replaced with css for sliders. Added Arrow

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-scroll";
 // Importing components
 import DownButton from "./sub-components/DownButton.jsx";
@@ -8,22 +8,6 @@ import github from "../assets/svg/github.svg";
 import linkedin from "../assets/svg/linkedin.svg";
 
 export default function Intro() {
-  const [githubHovered, setGithubHovered] = useState(false);
-  const [linkedinHovered, setLinkedinHovered] = useState(false);
-  const [contactHovered, setContactHovered] = useState(false);
-
-  const handleGithubHover = () => {
-    setGithubHovered(!githubHovered);
-  };
-
-  const handleLinkedinHover = () => {
-    setLinkedinHovered(!linkedinHovered);
-  };
-
-  const handleContactHover = () => {
-    setContactHovered(!contactHovered);
-  };
-
   const handleContactClick = () => {
     window.scrollTo({
       top: 1000,
@@ -33,14 +17,14 @@ export default function Intro() {
 
   return (
     <section id="intro" className="flex justify-center mb-48">
-      <div className="grid grid-cols-2 md:gap-10 gap-2">
+      <div className="xl:ml-16 grid grid-cols-2 md:gap-10 gap-2">
         <div className="flex flex-col items-center">
           <h1 className="xl:text-6xl md:text-5xl text-3xl mb-6">Ryan Harang</h1>
           <p className="xl:text-3xl md:text-2xl text-xl mb-6 text-center">
             Software Developer
           </p>
           <p className="xl:text-3xl md:text-xl text-lg mb-6 text-center">
-            Web Development and Design Enthusiast
+            Web Development and Design Specialist
           </p>
           <DownButton />
         </div>
@@ -49,50 +33,31 @@ export default function Intro() {
             href="https://github.com/RyanHarang"
             target="_blank"
             rel="noopener"
-            className="border-2 rounded sm:w-64 w-44 h-16 mb-8 flex items-center overflow-hidden duration-1000 hover:bg-theme-hover"
-            onMouseEnter={handleGithubHover}
-            onMouseLeave={handleGithubHover}
+            className="slider-container border-2 rounded h-16 mb-8 flex items-center overflow-hidden duration-1000 hover:bg-theme-hover"
           >
-            <img
-              src={github}
-              alt="Github"
-              className="h-16 transition-transform duration-700"
-              style={{
-                transform: githubHovered
-                  ? "translateX(12.1rem)"
-                  : "translateX(0)",
-              }}
-            />
+            <img src={github} alt="Github" className="slider h-16" />
             <h2 className="xl:text-2xl ml-3">Github</h2>
+            <p className="arrow sm:inline hidden w-full text-4xl text-end mr-3">
+              &rarr;
+            </p>
           </a>
           <br />
           <a
             href="https://www.linkedin.com/in/ryan-harang-a4461320a/"
             target="_blank"
             rel="noopener"
-            className="border-2 rounded sm:w-64 w-44 h-16 mb-8 flex items-center overflow-hidden duration-1000 hover:bg-theme-hover"
-            onMouseEnter={handleLinkedinHover}
-            onMouseLeave={handleLinkedinHover}
+            className="slider-container border-2 rounded h-16 mb-8 flex items-center overflow-hidden duration-1000 hover:bg-theme-hover"
           >
-            <img
-              src={linkedin}
-              alt="LinkedIn"
-              className="h-16 transition-transform duration-700"
-              style={{
-                transform: linkedinHovered
-                  ? "translateX(12.1rem)"
-                  : "translateX(0)",
-              }}
-            />
+            <img src={linkedin} alt="LinkedIn" className="slider h-16" />
             <h2 className="xl:text-2xl ml-3">LinkedIn</h2>
+            <p className="arrow sm:inline hidden w-full text-4xl text-end mr-3">
+              &rarr;
+            </p>
           </a>
           <br />
           <Link
             onClick={handleContactClick}
-            className="border-2 rounded sm:w-64 w-44 h-16 mb-5 flex items-center overflow-hidden duration-1000 hover:bg-theme-hover"
-            onMouseEnter={handleContactHover}
-            onMouseLeave={handleContactHover}
-            activeClass="active"
+            className="slider-container border-2 rounded h-16 mb-5 flex items-center overflow-hidden duration-1000 hover:bg-theme-hover"
             role="button"
             tabIndex="0"
             to="contact"
@@ -100,17 +65,11 @@ export default function Intro() {
             smooth={true}
             duration={500}
           >
-            <img
-              src={email}
-              alt="Contact me"
-              className="h-16 transition-transform duration-700"
-              style={{
-                transform: contactHovered
-                  ? "translateX(12.1rem)"
-                  : "translateX(0)",
-              }}
-            />
+            <img src={email} alt="Contact me" className="slider h-16" />
             <h2 className="xl:text-2xl ml-3">Contact</h2>
+            <p className="arrow sm:inline hidden w-full text-4xl text-end mr-3">
+              &rarr;
+            </p>
           </Link>
         </div>
       </div>
