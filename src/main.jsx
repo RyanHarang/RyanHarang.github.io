194de434b78479e344b195f8f79de8e1c0dd64a7
Ryan <ryh110.rh@gmail.com>
2024-06-03 08:53:56 -0700

    adding new css import

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./css/index.css";
 import "./css/waves.css";
+import "./css/slider.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
