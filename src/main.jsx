e0c71d673d1ebc99a0cb8fdeaff1c61b4f77fda4
Ryan <ryh110.rh@gmail.com>
2024-12-08 13:37:23 -0800

    Modifying scrollbar thickness

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./css/index.css";
-import "./css/waves.css";
-import "./css/slider.css";
 import "./css/modeToggle.css";
+import "./css/scrollbar.css";
+import "./css/slider.css";
+import "./css/waves.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
