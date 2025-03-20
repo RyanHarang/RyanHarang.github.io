4c953586917d89077d9eccd75edf5354aeb10add
RyanHarang <ryh110.rh@gmail.com>
2025-03-20 08:55:30 -0700

    svg for mobile hamburger menu

---
+++ src/assets/svg/hamburger/Hamburger.jsx
@@ -0,0 +1,21 @@
+const Hamburger = () => {
+  return (
+    <svg
+      width="800"
+      height="800"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="fill-light-fg dark:fill-dark-fg h-6 w-6"
+      role="img"
+      aria-label="Toggle hamburger menu"
+      focusable="false"
+    >
+      <path d="M5 6.5H19V8H5V6.5Z" />
+      <path d="M5 16.5H19V18H5V16.5Z" />
+      <path d="M5 11.5H19V13H5V11.5Z" />
+    </svg>
+  );
+};
+
+export default Hamburger;
