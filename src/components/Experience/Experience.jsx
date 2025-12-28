bd56c5cd6557ee00d1410561b0e19379bd2e5bdc
RyanHarang <ryh110.rh@gmail.com>
2025-12-28 06:35:52 -0800

    update logos bg

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,30 +1,32 @@
 const Experience = ({ company, image, title, link, date, description }) => {
-  return (
-    <div className="dark:shadow-dark-fg2/30 mb-4 rounded-sm border p-6 shadow-lg">
-      <div className="mb-1 flex items-center">
-        <img
-          src={image}
-          alt={company}
-          draggable="false"
-          loading="lazy"
-          className="mr-2 h-16 w-16 rounded-full border object-contain"
-        />
-        <h3 className="text-xl font-bold underline">
-          <a
-            href={link}
-            target="_blank"
-            draggable="false"
-            rel="noopener noreferrer"
-          >
-            {company}
-          </a>
-        </h3>
-      </div>
-      <p className="text-light-fg2 dark:text-dark-fg2">{title} </p>
-      <p className="text-light-fg2 dark:text-dark-fg3 mb-2 text-sm">{date}</p>
-      <p className="text-base">{description}</p>
-    </div>
-  );
+    return (
+        <div className="dark:shadow-dark-fg2/30 mb-4 rounded-sm border p-6 shadow-lg">
+            <div className="mb-1 flex items-center">
+                <img
+                    src={image}
+                    alt={company}
+                    draggable="false"
+                    loading="lazy"
+                    className="mr-2 h-16 w-16 rounded-full border object-contain dark:bg-light-bg"
+                />
+                <h3 className="text-xl font-bold underline">
+                    <a
+                        href={link}
+                        target="_blank"
+                        draggable="false"
+                        rel="noopener noreferrer"
+                    >
+                        {company}
+                    </a>
+                </h3>
+            </div>
+            <p className="text-light-fg2 dark:text-dark-fg2">{title} </p>
+            <p className="text-light-fg2 dark:text-dark-fg3 mb-2 text-sm">
+                {date}
+            </p>
+            <p className="text-base">{description}</p>
+        </div>
+    );
 };
 
 export default Experience;
