6410893462626cbdf72f1c4403ac05e30474c495
RyanHarang <ryh110.rh@gmail.com>
2026-06-18 22:10:21 -0700

    Took down weather app

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,68 +1,68 @@
 const projects = [
-  {
-    image: "/images/projects/localMusicStreamer.webp",
-    name: "Local Music Streamer",
-    link: "https://github.com/RyanHarang/Local-Music-Streamer/tree/main",
-    description:
-      "Created a streaming platform for local audio files with similar capabilties to major streaming services. Actively developing and creating Electron based desktop application for easier use.",
-    skills: ["React", "Electron", "Node.js", "Tailwind"],
-  },
-  {
-    image: "/images/projects/data1.webp",
-    name: "Data 1: Formula 1 Data Aggregator",
-    link: "https://github.com/RyanHarang/Formula-1-Data-1",
-    description:
-      "Data 1 is a group project created to allow users to explore and compare historic data from Formula 1 races and drivers.",
-    skills: [
-      "React",
-      "Node.js",
-      "Express",
-      "MongoDB",
-      "AWS",
-      "Tailwind",
-      "Figma",
-    ],
-  },
-  {
-    image: "/images/projects/classSync.webp",
-    name: "ClassSync",
-    link: "https://github.com/RyanHarang/ClassSync",
-    description:
-      "Created a browser extension that interacted with the Canvas API to allow users to quickly view their schedule at any time and add it to external calendars.",
-    skills: ["HTML", "CSS", "JavaScript", "APIs"],
-  },
-  {
-    image: "/images/projects/gameRating.webp",
-    name: "Game Rating Site",
-    link: "https://github.com/RyanHarang/game-rating",
-    description:
-      "Created a full-stack web application including user differentiation for guests, users and admins, dynamic data display drawn from a MongoDB database and AWS S3 bucket, and posting of additional data to the backend via forms. This project utilized the MERN stack for developement and is hosted via Render.",
-    skills: [
-      "React",
-      "Node.js",
-      "Express",
-      "MongoDB",
-      "AWS",
-      "Tailwind",
-      "Render",
-    ],
-  },
-  {
-    image: "/images/projects/weatherApp.webp",
-    name: "Weather Website",
-    link: "https://weather-application-4245c.web.app/",
-    description:
-      "Initially, I leveraged the OpenWeatherMap API to access weather data around the world. Simultaneously, I accessed data from GeoNames, which supplied a list containing the names of cities worldwide. I parsed the file and implemented a filtering mechanism to construct the list that users can access for weather information. The application is now deployed via Firebase.",
-    skills: ["React", "Firebase", "APIs"],
-  },
-  {
-    image: "/images/projects/pente.webp",
-    name: "Pente Web-App",
-    link: "https://pente-31410.web.app/",
-    description:
-      "I first implemented Pente in Java as a text-user-interface, and later recreated the game with a graphical-user-interface through React. The application is now deployed via Firebase.",
-    skills: ["React", "Java", "Firebase"],
-  },
+    {
+        image: "/images/projects/localMusicStreamer.webp",
+        name: "Local Music Streamer",
+        link: "https://github.com/RyanHarang/Local-Music-Streamer/tree/main",
+        description:
+            "Created a streaming platform for local audio files with similar capabilties to major streaming services. Actively developing and creating Electron based desktop application for easier use.",
+        skills: ["React", "Electron", "Node.js", "Tailwind"],
+    },
+    {
+        image: "/images/projects/data1.webp",
+        name: "Data 1: Formula 1 Data Aggregator",
+        link: "https://github.com/RyanHarang/Formula-1-Data-1",
+        description:
+            "Data 1 is a group project created to allow users to explore and compare historic data from Formula 1 races and drivers.",
+        skills: [
+            "React",
+            "Node.js",
+            "Express",
+            "MongoDB",
+            "AWS",
+            "Tailwind",
+            "Figma",
+        ],
+    },
+    {
+        image: "/images/projects/classSync.webp",
+        name: "ClassSync",
+        link: "https://github.com/RyanHarang/ClassSync",
+        description:
+            "Created a browser extension that interacted with the Canvas API to allow users to quickly view their schedule at any time and add it to external calendars.",
+        skills: ["HTML", "CSS", "JavaScript", "APIs"],
+    },
+    {
+        image: "/images/projects/gameRating.webp",
+        name: "Game Rating Site",
+        link: "https://github.com/RyanHarang/game-rating",
+        description:
+            "Created a full-stack web application including user differentiation for guests, users and admins, dynamic data display drawn from a MongoDB database and AWS S3 bucket, and posting of additional data to the backend via forms. This project utilized the MERN stack for developement and is hosted via Render.",
+        skills: [
+            "React",
+            "Node.js",
+            "Express",
+            "MongoDB",
+            "AWS",
+            "Tailwind",
+            "Render",
+        ],
+    },
+    {
+        image: "/images/projects/weatherApp.webp",
+        name: "Weather Website",
+        link: "https://github.com/RyanHarang/Weather-App/tree/main",
+        description:
+            "Leveraging the OpenWeatherMap API, I accessed weather data around the world. Using data from GeoNames, I implemented a filtering mechanism to construct a list of cities that met a 15000 population threshold that users can access for weather information. The application was deployed for a time via Firebase. ",
+        skills: ["React", "Firebase", "APIs"],
+    },
+    {
+        image: "/images/projects/pente.webp",
+        name: "Pente Web-App",
+        link: "https://pente-31410.web.app/",
+        description:
+            "I first implemented Pente in Java as a text-user-interface, and later recreated the game with a graphical-user-interface through React. The application is now deployed via Firebase.",
+        skills: ["React", "Java", "Firebase"],
+    },
 ];
 
 export default projects;
