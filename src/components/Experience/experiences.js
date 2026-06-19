657f7451dd6c97ecbef20fedac941057ede2b367
RyanHarang <ryh110.rh@gmail.com>
2026-06-18 22:12:38 -0700

    Job description update

--- src/components/Experience/experiences.js
+++ src/components/Experience/experiences.js
@@ -1,49 +1,49 @@
 const experiences = [
-  {
-    image: "/images/experience/strivve.png",
-    company: "Strivve",
-    title: "Associate Software Engineer",
-    link: "https://strivve.com/",
-    date: "October 2025 - Present",
-    description:
-      "Working as an Associate Software Engineer at Strivve, contributing to the development of CardUpdatr, a platform that automates credit card updates for users.",
-  },
-  {
-    image: "/images/experience/applied.png",
-    company: "Applied Medical",
-    title: "Full Stack Software Engineer Intern",
-    link: "https://www.appliedmedical.com/",
-    date: "June 2024 - August 2024",
-    description:
-      "For ten weeks I worked at Applied Medical as a full stack Software Engineer and contributed to numerous projects. Details of my work can be found on my resume.",
-  },
-  {
-    image: "/images/experience/wwu.svg",
-    company: "Western Washington University",
-    title: "CS Teaching Assistant",
-    link: "https://www.wwu.edu/",
-    date: "September 2023 - Present",
-    description:
-      "Independently led lab sessions, teaching HTML, CSS and javascript, as well as grading submissions.",
-  },
-  {
-    image: "/images/experience/wwu.svg",
-    company: "Western Washington University",
-    title: "CS Research Assistant",
-    link: "https://www.wwu.edu/",
-    date: "April 2023 - June 2024",
-    description:
-      "Spearheaded the transfer of site content to React and improved user experience for the Index In-Bounds Lab. Then worked on curating the back-end with a Django database to publish lab work.",
-  },
-  {
-    image: "/images/experience/theLadder.png",
-    company: "The Ladder",
-    title: "Technical Intern",
-    link: "https://theladderredruth.com/",
-    date: "June 2023 - July 2023",
-    description:
-      "Led a collaborative effort with a non-profit, The Ladder to develop an interactive audio system run through Alexa. Used Firebase to store a graph structure that determined the flow of audio based on user utterances.",
-  },
+    {
+        image: "/images/experience/strivve.png",
+        company: "Strivve",
+        title: "Associate Software Engineer",
+        link: "https://strivve.com/",
+        date: "October 2025 - Present",
+        description:
+            "Working as an Associate Software Engineer at Strivve, contributing to the development of CardUpdatr, a platform that automates merchant site credit card updates. Specializing in large scale web automation.",
+    },
+    {
+        image: "/images/experience/applied.png",
+        company: "Applied Medical",
+        title: "Full Stack Software Engineer Intern",
+        link: "https://www.appliedmedical.com/",
+        date: "June 2024 - August 2024",
+        description:
+            "For ten weeks I worked at Applied Medical as a full stack Software Engineer and contributed to numerous projects. Details of my work can be found on my resume.",
+    },
+    {
+        image: "/images/experience/wwu.svg",
+        company: "Western Washington University",
+        title: "CS Teaching Assistant",
+        link: "https://www.wwu.edu/",
+        date: "September 2023 - Present",
+        description:
+            "Independently led lab sessions, teaching HTML, CSS and javascript, as well as grading submissions.",
+    },
+    {
+        image: "/images/experience/wwu.svg",
+        company: "Western Washington University",
+        title: "CS Research Assistant",
+        link: "https://www.wwu.edu/",
+        date: "April 2023 - June 2024",
+        description:
+            "Spearheaded the transfer of site content to React and improved user experience for the Index In-Bounds Lab. Then worked on curating the back-end with a Django database to publish lab work.",
+    },
+    {
+        image: "/images/experience/theLadder.png",
+        company: "The Ladder",
+        title: "Technical Intern",
+        link: "https://theladderredruth.com/",
+        date: "June 2023 - July 2023",
+        description:
+            "Led a collaborative effort with a non-profit, The Ladder to develop an interactive audio system run through Alexa. Used Firebase to store a graph structure that determined the flow of audio based on user utterances.",
+    },
 ];
 
 export default experiences;
